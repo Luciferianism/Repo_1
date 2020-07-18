a9f8017929306dbd863de8406471f18f3a5b1e96
Luciferianism <buttowskik6@gmail.com>
2020-07-18 18:17:44 +0530

    adding oops structure approach

---
+++ comm_1.py
@@ -0,0 +1,10 @@
+import os
+class master(object):
+    def __init__(self) -> None:
+        pass
+
+    def foo_1(self):
+        pass
+
+    def foo_2(self):
+        pass
---
+++ comm_2.py
@@ -0,0 +1,14 @@
+import os
+from comm_1 import master
+class base(master):
+    def __init__(self) -> None:
+        pass
+
+def main():
+    obj = base()
+    for i in range(1):
+        pass
+    pass
+
+if __name__ == "__main__":
+    pass
